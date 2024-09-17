bfe82d075f15429144d52324d100b4ebe07be888
andrewjohnsonwijaya <andrewjohnsonwijaya>
2024-09-17 12:28:22 +0700

    edit ui dari file upload_image

--- lib/pages/upload_image.dart
+++ lib/pages/upload_image.dart
@@ -13,12 +13,68 @@ class _UploadImageState extends State<UploadImage> {
     return const Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        child: Stack(
+        child: Column(
           children: [
+            Text(
+              '',
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: Colors.blue,
+              ),
+            ),
+            Text(
+              'Click circle below to add photo',
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: Colors.blue,
+              ),
+            ),
+            Text(
+              '',
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: Colors.blue,
+              ),
+            ),
             CircleAvatar(
               radius: 100,
               backgroundColor: Color(0xFF4285F4),
+              child: Text('click me'),
             ),
+            Text(
+              '',
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: Colors.blue,
+              ),
+            ),
+            Text(
+              'Add caption below',
+              style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 25,
+              color: Colors.blue,
+              ),
+            ),
+            Card(
+              color: Colors.blue,
+              margin: EdgeInsets.symmetric(
+                horizontal: 20.0,
+                vertical: 20.0,
+              ),
+              child: ListTile(
+                leading: Icon(Icons.message,
+                    color: Colors.black),
+                title: Text(
+                  'write your caption here',
+                   style: TextStyle(color: Colors.black),
+                 ),
+               ),
+             ),
           ],
         ),
       ),
